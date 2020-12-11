48af8009601eecc5a7412a6b323d758a6fff34c9
Sherry Wu <36207704+sherryxw@users.noreply.github.com>
2020-12-11 12:59:47 -0800

    sherry-fix

--- src/components/search/SearchPageHeader.js
+++ src/components/search/SearchPageHeader.js
@@ -1,28 +1,13 @@
 import React from "react"
-import "./search.style.client.css"
-import image from "./header.jpg"
+import "./searchPageHeader.style.client.css"
+
 const SearchPageHeader = () =>
-    <div className="container">
-        <div className="row">
-            <div className="col-2">
-                <img className="ham-adj" src="https://lh3.googleusercontent.com/proxy/NDJRpviVy9HTIPUFlATuWVHRK71jn_uJhwiB2igZfiBqGgNyHTMtwWZXS_50jqtj7rQF3lE8LFLIDvT-HfJJBszlbGQKgUBtseoJJ3-vuZHwaMcdJFizyY9rf2YA" alt="ham"/>
-            </div>
-            <div className="col-2">
-                <img className="ham-adj" src="https://static.wixstatic.com/media/2cd43b_41060979967b4216b4d821c66fa49da2~mv2.png/v1/fill/w_259,h_203,fp_0.50_0.50/2cd43b_41060979967b4216b4d821c66fa49da2~mv2.png" alt="sushi"/>
-            </div>
-            <div className="col-2">
-                <img className="ham-adj" src="https://www.pngitem.com/pimgs/m/97-978762_transparent-fortune-cookie-png-chinese-dim-sum-clipart.png" alt="ham"/>
-            </div>
-            <div className="col-2">
-                <img className="ham-adj" src="https://lh3.googleusercontent.com/proxy/3qp8S8BgOC-eAh93vzXfYUsEZ3PWnPuZtqeetqDm0hHGK3cG1avyRjsxJxvLIfMvuOwirtj0FOwSJ5_DhXxJpmwPUxOKKbS-6ITrCCvt" alt="ham"/>
-            </div>
-            <div className="col-2">
-                <img className="ham-adj" src="https://lh3.googleusercontent.com/proxy/BVjA4iT_EsTYZq0Ref2QEakdF7fz8g-St9a3DTGdeMRa33O__3NQu2lrbcI1H0pJOK2w6sivi9p3cYA1H2PcU7VUy2qHy0eLuAUIiY5PgB4Q0c7TPbCiYsGZ6wSkpUlWkt7wCQqbCDS4qE4" alt="ham"/>
-            </div>
-            <div className="col-2">
-                <img className="ham-adj" src="https://png.pngtree.com/png-clipart/20190614/original/pngtree-korean-food--bibimbap-png-image_3642727.jpg" alt="ham"/>
-            </div>
-        </div>
-        </div>
+    <div>
+        <nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">
+
+            </a>
+        </nav>
+    </div>
 
 export default  SearchPageHeader
